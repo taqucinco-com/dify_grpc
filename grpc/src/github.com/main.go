f34f1d2b0d57fee6744a5d8cb15ff7afac825bec
taqucinco <someone.said.so@gmail.com>
2024-09-08 21:43:33 +0900

    streamingモードからメッセージを取り出す

--- grpc/src/github.com/main.go
+++ grpc/src/github.com/main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/joho/godotenv"
 	sse "github.com/tmaxmax/go-sse"
 )
+
+type EventData struct {
+	Event  string  `json:"event"`
+	TaskID string  `json:"task_id"`
+	Answer *string `json:"answer"`
+}
 
 func main() {
 	err := godotenv.Load()
@@ -20,32 +30,50 @@ func main() {
 		return
 	}
 	key := os.Getenv("DIFY_API_KEY")
-	fmt.Println(key)
+	// fmt.Println(key)
 
-	client := sse.Client{
+	sseClient := sse.Client{
 		Backoff: sse.Backoff{
 			MaxRetries: -1,
 		},
 	}
-	json := `{
-	    "inputs": {},
-    	"query": "What are the specs of the iPhone 13 Pro Max?",                  
-    	"response_mode": "streaming",
-    	"conversation_id": "",
-    	"user": "user-123",
-    	"files": []
+	userId := "user-123"
+	body := `{
+		"inputs": {},
+		"query": "What are the specs of the iPhone 13 Pro Max?",                  
+		"response_mode": "streaming",
+		"conversation_id": "",
+		"user": "` + userId + `",
+		"files": []
 	}`
-	req, err := http.NewRequest(http.MethodPost, "https://api.dify.ai/v1/chat-messages", bytes.NewBuffer([]byte(json)))
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+	difyReq, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://api.dify.ai/v1/chat-messages", bytes.NewBuffer([]byte(body)))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error creating request: %v", err)
+		return
 	}
-	req.Header.Set("Authorization", "Bearer "+key)
-	req.Header.Set("Content-Type", "application/json")
+	difyReq.Header.Set("Authorization", "Bearer "+key)
+	difyReq.Header.Set("Content-Type", "application/json")
 
-	conn := client.NewConnection(req)
+	conn := sseClient.NewConnection(difyReq)
 
 	unsubscribe := conn.SubscribeMessages(func(ev sse.Event) {
-		fmt.Printf(ev.Data + "\n")
+		// fmt.Printf(ev.Data + "\n")
+		var eventData EventData
+		err := json.Unmarshal([]byte(ev.Data), &eventData)
+		if err == nil {
+			eventName := eventData.Event
+			taskID := eventData.TaskID
+
+			if eventName == "workflow_started" {
+				fmt.Println("Task ID:", taskID)
+			} else if eventName == "message" {
+				answer := *eventData.Answer
+				fmt.Println("Answer:", answer)
+			}
+		}
+
 	})
 	defer unsubscribe()
 

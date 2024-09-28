9492d8b8f47ddf6948a29aa8d3c5844da5e7acde
takuya.sudo <takuya.sudo@livefor.jp>
2024-09-29 01:31:13 +0900

    iosプロジェクトを作り直す

---
+++ ios/grpc_sample/grpc_sampleTests/grpc_sampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  grpc_sampleTests.swift
+//  grpc_sampleTests
+//
+//  Created by takuya.sudo on 2024/09/29.
+//
+
+import Testing
+@testable import grpc_sample
+
+struct grpc_sampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ios/grpc_sample/grpc_sampleUITests/grpc_sampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  grpc_sampleUITestsLaunchTests.swift
+//  grpc_sampleUITests
+//
+//  Created by takuya.sudo on 2024/09/29.
+//
+
+import XCTest
+
+final class grpc_sampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
